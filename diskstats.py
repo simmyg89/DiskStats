b565ccaecb62e937b572abd2b4241a7b1e5d3fa1
Simone <simmyg89@gmail.com>
2013-07-08 11:17:22 +0200

    Moved to argparse and added legend for colums values

--- diskstats.py
+++ diskstats.py
@@ -1,48 +1,60 @@
 #!/usr/bin/env python
 
-from optparse import OptionParser
+import argparse
 
 
-def ignore_callback(option, opt, value, parser):
-    setattr(parser.values, option.dest, value.split(','))
+class ignore_callback(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, value.split(','))
 
-parser = OptionParser()
-parser.add_option('-s', '--spaces', type='int', dest='SPACES', default=0, help='Spaces between colums')
-parser.add_option('-i', '--ignore', type='string', dest='IGNORE_DEVS', default=['loop', 'ram'], help='Devices to ignore', action='callback', callback=ignore_callback)
-parser.add_option('-o', '--only', dest='ONLY_DEV', help='Only show the specified device')
-(options, args) = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--spaces', type=int, dest='SPACES', default=0, help='Spaces between colums')
+parser.add_argument('-i', '--ignore', type=str, dest='IGNORE_DEVS', default=['loop', 'ram'], help='Devices to ignore', action=ignore_callback)
+parser.add_argument('-o', '--only', dest='ONLY_DEV', help='Only show the specified device')
+parser.add_argument('-l', '--legend', dest='LEGEND', default=False, action="store_true", help='Show columns legend')
+args = parser.parse_args()
 
-#print 'options: %s' % options.__dict__
 #print 'args: %s' % args
 
-SPACES = int(options.__dict__.get('SPACES'))
-IGNORE_DEVS = options.__dict__.get('IGNORE_DEVS')
-ONLY_DEV = options.__dict__.get('ONLY_DEV')
+SPACES = int(args.__dict__.get('SPACES'))
+IGNORE_DEVS = args.__dict__.get('IGNORE_DEVS')
+ONLY_DEV = args.__dict__.get('ONLY_DEV')
+LEGEND = args.__dict__.get('LEGEND')
 
 #print 'SPACES: %s' % SPACES
 #print 'IGNORE_DEVS: %s' % IGNORE_DEVS
 #print 'ONLY_DEV: %s' % ONLY_DEV
+#print 'LEGEND: %s' % LEGEND
 
 dsfp = open('/proc/diskstats', 'rb')
 ds = dsfp.read().split('\n')
 
 header = {
-    0: {'h': 'm', 'l': 1},
-    1: {'h': 'm', 'l': 1},
-    2: {'h': 'dev', 'l': 3},
-    3: {'h': 'reads', 'l': 5},
-    4: {'h': 'rd_mrg', 'l': 6},
-    5: {'h': 'rd_sectors', 'l': 10},
-    6: {'h': 'ms_reading', 'l': 10},
-    7: {'h': 'writes', 'l': 6},
-    8: {'h': 'wr_mrg', 'l': 6},
-    9: {'h': 'wr_sectors', 'l': 10},
-    10: {'h': 'ms_writing', 'l': 10},
-    11: {'h': 'cur_ios', 'l': 7},
-    12: {'h': 'ms_doing_io', 'l': 11},
-    13: {'h': 'ms_weighted', 'l': 11},
+    0: {'h': 'n_ma', 'l': 4, 'd': 'Node Major'},
+    1: {'h': 'n_mi', 'l': 4, 'd': 'Node Minor'},
+    2: {'h': 'dev', 'l': 3, 'd': 'Device'},
+    3: {'h': 'reads', 'l': 5, 'd': 'Reads Issued'},
+    4: {'h': 'rd_mrg', 'l': 6, 'd': 'Reads Merged'},
+    5: {'h': 'rd_sectors', 'l': 10, 'd': 'Sectors Read'},
+    6: {'h': 'ms_reading', 'l': 10, 'd': 'Milliseconds Spent Reading'},
+    7: {'h': 'writes', 'l': 6, 'd': 'Writes Completed'},
+    8: {'h': 'wr_mrg', 'l': 6, 'd': 'Writes Merged'},
+    9: {'h': 'wr_sectors', 'l': 10, 'd': 'Sectors Written'},
+    10: {'h': 'ms_writing', 'l': 10, 'd': 'Milliseconds Spent Writing'},
+    11: {'h': 'cur_ios', 'l': 7, 'd': 'I/Os currently in Progress'},
+    12: {'h': 'ms_doing_io', 'l': 11, 'd': 'Milliseconds Spent Doing I/Os'},
+    13: {'h': 'ms_weighted', 'l': 11, 'd': 'Weighted Milliseconds Spent Doing I/Os '},
 }
 #print header
+
+if LEGEND:
+    print ''
+    print 'LEGEND :'
+    maxl = max([header[x]['l'] for x in header])
+    for p in header:
+        print '%s' % header[p]['h'],
+        print ' ' * (maxl - len(header[p]['h'])),
+        print '-> %s' % header[p]['d']
 
 disks = {}
 
